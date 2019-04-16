cc8c7b24bfc304487ccefdd4b73b2353f6477548
Chris Fethke <christopher_fethke@brown.edu>
2019-04-16 16:12:06 -0400

    fixed regex pattern to prevent crash

--- profile.js
+++ profile.js
@@ -6,11 +6,7 @@
 /*										*/
 /********************************************************************************/
 
-
 var db = require("./database.js");
-
-
-
 
 /********************************************************************************/
 /*										*/
@@ -18,42 +14,32 @@ var db = require("./database.js");
 /*										*/
 /********************************************************************************/
 
-function displayProfile(req,res,next)
-{
-   displayProfile0(req,res,next,false);
+function displayProfile(req, res, next) {
+  displayProfile0(req, res, next, false);
 }
 
+function displayProfile0(req, res, next, succ) {
+  var userid = req.session.userId;
 
+  var q =
+    "SELECT U.firstName, U.lastName, P.ssn, P.dob, P.address, P.bankAcc, P.bankRouting";
+  q += " FROM User U, Profile P";
+  q += " WHERE U.userId = P.userId AND U.userId = " + userid;
 
-
-function displayProfile0(req,res,next,succ)
-{
-   var userid = req.session.userId;
-
-   var q = "SELECT U.firstName, U.lastName, P.ssn, P.dob, P.address, P.bankAcc, P.bankRouting";
-   q += " FROM User U, Profile P";
-   q += " WHERE U.userId = P.userId AND U.userId = " + userid;
-
-   db.query(q,function (e1,d1) { displayProfile1(req,res,next,succ,e1,d1); } );
+  db.query(q, function(e1, d1) {
+    displayProfile1(req, res, next, succ, e1, d1);
+  });
 }
 
+function displayProfile1(req, res, next, succ, err, data) {
+  if (err != null) return next(err);
 
+  var doc = data.rows[0];
+  doc.userId = req.session.userId;
+  if (succ) doc.updateSuccess = true;
 
-
-function displayProfile1(req,res,next,succ,err,data)
-{
-   if (err != null) return next(err);
-
-   var doc = data.rows[0];
-   doc.userId = req.session.userId;
-   if (succ) doc.updateSuccess = true;
-   
-   return res.render("profile",doc);
+  return res.render("profile", doc);
 }
-
-
-
-
 
 /********************************************************************************/
 /*										*/
@@ -61,61 +47,96 @@ function displayProfile1(req,res,next,succ,err,data)
 /*										*/
 /********************************************************************************/
 
-function handleProfileUpdate(req,res,next)
-{
-   var firstname = req.body.firstName;
-   var lastname = req.body.lastName;
-   var ssn = req.body.ssn;
-   var dob = req.body.dob;
-   var address = req.body.address;
-   var bankAcc = req.body.bankAcc;
-   var bankRouting = req.body.bankRouting;
+function handleProfileUpdate(req, res, next) {
+  var firstname = req.body.firstName;
+  var lastname = req.body.lastName;
+  var ssn = req.body.ssn;
+  var dob = req.body.dob;
+  var address = req.body.address;
+  var bankAcc = req.body.bankAcc;
+  var bankRouting = req.body.bankRouting;
 
-   var regexPattern = /([0-9]+)+\#/;
-   // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
-   var testComplyWithRequirements = regexPattern.test(bankRouting);
-   if (testComplyWithRequirements !== true) {
-      var doc = { updateError: "Bank Routing number does not comply with requirements for format specified" };
-      return res.render("profile", doc);
-    }
+  //var regexPattern = /([0-9]+)+\#/;
 
-   var userId = req.session.userId;
+  // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
+  // var testComplyWithRequirements = regexPattern.test(bankRouting);
 
-   var q = "UPDATE User SET firstName = '" + firstname + "', lastName = '" + lastname + "'" +
-      " WHERE userId = " + userId;
-   db.query(q,function (e1,d1) { handleProfileUpdate1(req,res,next,e1,d1); } );
+  var testComplyWithRequirements = checkBankRoutingNumber(bankRouting);
+  
+  if (testComplyWithRequirements !== true) {
+    var doc = {
+      updateError:
+        "Bank Routing number does not comply with requirements for format specified"
+    };
+    return res.render("profile", doc);
+  }
+
+  var userId = req.session.userId;
+
+  var q =
+    "UPDATE User SET firstName = '" +
+    firstname +
+    "', lastName = '" +
+    lastname +
+    "'" +
+    " WHERE userId = " +
+    userId;
+  db.query(q, function(e1, d1) {
+    handleProfileUpdate1(req, res, next, e1, d1);
+  });
 }
 
+function handleProfileUpdate1(req, res, next, err, data) {
+  if (err != null) return next(err);
 
+  var ssn = req.body.ssn;
+  var dob = req.body.dob;
+  var address = req.body.address;
+  var bankAcc = req.body.bankAcc;
+  var bankRouting = req.body.bankRouting;
 
-function handleProfileUpdate1(req,res,next,err,data)
-{
-   if (err != null) return next(err);
+  var q =
+    "UPDATE Profile SET ssn = '" +
+    ssn +
+    "', dob = '" +
+    dob +
+    "', address = '" +
+    address +
+    "', bankAcc = '" +
+    bankAcc +
+    "', bankRouting = '" +
+    bankRouting +
+    "'" +
+    " WHERE userId = " +
+    req.session.userId;
 
-   var ssn = req.body.ssn;
-   var dob = req.body.dob;
-   var address = req.body.address;
-   var bankAcc = req.body.bankAcc;
-   var bankRouting = req.body.bankRouting;
-
-   var q = "UPDATE Profile SET ssn = '" + ssn + "', dob = '" + dob + "', address = '" +
-      address + "', bankAcc = '" + bankAcc + "', bankRouting = '" + bankRouting + "'" +
-      " WHERE userId = " + req.session.userId;
-
-   db.query(q,function(e1,d1) { handleProfileUpdate2(req,res,next,e1,d1); } );
+  db.query(q, function(e1, d1) {
+    handleProfileUpdate2(req, res, next, e1, d1);
+  });
 }
 
+function handleProfileUpdate2(req, res, next, err, data) {
+  if (err != null) return next(err);
 
-
-function handleProfileUpdate2(req,res,next,err,data)
-{
-   if (err != null) return next(err);
-
-   displayProfile0(req,res,next,true);
+  displayProfile0(req, res, next, true);
 }
 
+const checkBankRoutingNumber = routingNumber => {
+  const length = routingNumber.length;
+  // Check if last character is a `#`
+  if (routingNumber[length - 1] != "#") {
+    return false;
+  }
 
+  const digitsPart = routingNumber.substring(0, length - 1);
+  // Check if rest of routingNumber 1 or more digits and only digits:
+  const regexPattern = /^[0-9]*$/;
+  if (!regexPattern.test(digitsPart)) {
+    return false;
+  }
 
+  return true;
+};
 
 /********************************************************************************/
 /*										*/
@@ -125,8 +146,5 @@ function handleProfileUpdate2(req,res,next,err,data)
 
 exports.displayProfile = displayProfile;
 exports.handleProfileUpdate = handleProfileUpdate;
-
-
-
 
 /* end of profile.js */
